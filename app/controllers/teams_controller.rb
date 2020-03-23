664678cae4d0cda9f552851879b0365cf132d2a7
hiroki matsubara <dr5se4aw3@yahoo.com>
2020-03-23 11:10:05 +0900

    Update チームの編集はそのチームのリーダー以外できないようにする

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -15,7 +15,11 @@ class TeamsController < ApplicationController
     @team = Team.new
   end
 
-  def edit; end
+  def edit
+    unless current_user.id == @team.owner_id
+      redirect_to teams_url, notice:I18n.t('views.messages.leader_only_can_edit_team')
+    end
+  end
 
   def create
     @team = Team.new(team_params)
