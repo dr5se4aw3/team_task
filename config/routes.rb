1014152c8e13e407a50bf97727ddfc9268e46c02
hiroki matsubara <dr5se4aw3@yahoo.com>
2020-03-23 16:51:21 +0900

    Add Agendaの名前の右の部分に削除ボタンを作成し、そのボタンを押すとそのAgendaが削除される

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,11 @@ Rails.application.routes.draw do
     registrations: 'users/registrations',
     passwords: 'users/passwords'
   }
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   resource :user
-  
+
   resources :teams do
     resources :assigns, only: %w(create destroy)
     resources :agendas, shallow: true do
