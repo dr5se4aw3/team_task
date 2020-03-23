dfdb9acdd11172f990216ea375b86f8ba1954b51
hiroki matsubara <dr5se4aw3@yahoo.com>
2020-03-23 12:06:29 +0900

    Update チームメンバーの削除を、そのチームのリーダーとメンバー本人のみが出来るように変更

--- app/controllers/assigns_controller.rb
+++ app/controllers/assigns_controller.rb
@@ -14,6 +14,11 @@ class AssignsController < ApplicationController
 
   def destroy
     assign = Assign.find(params[:id])
+    binding.pry
+    unless (current_user.id == assign.user.id) || (current_user.id == assign.team.owner_id )
+      redirect_to team_url(params[:team_id]), notice: I18n.t('views.messages.cannot_delete_other_member')
+      return
+    end
     destroy_message = assign_destroy(assign, assign.user)
 
     redirect_to team_url(params[:team_id]), notice: destroy_message
@@ -37,13 +42,15 @@ class AssignsController < ApplicationController
       I18n.t('views.messages.cannot_delete_member_4_some_reason')
     end
   end
-  
+
   def email_reliable?(address)
     address.match(/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i)
   end
-  
+
   def set_next_team(assign, assigned_user)
     another_team = Assign.find_by(user_id: assigned_user.id).team
     change_keep_team(assigned_user, another_team) if assigned_user.keep_team_id == assign.team_id
   end
+
+
 end
